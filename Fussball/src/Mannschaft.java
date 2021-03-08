5327b89953a014761e24b7c13bdd38a461b8f569
milane <milane2@schule.bremen.de>
2021-03-08 10:14:06 +0100

    AB 2,3 ... 4 Nr. 3

---
+++ Fussball/src/Mannschaft.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Mannschaft {
+
+    String name;
+    Trainer trainer;
+    Torwart torwart;
+    private ArrayList<Spieler> spielerListe;
+
+
+    public Mannschaft(String Trainer, String Torwart, ArrayList <Spieler> spielerListe ){
+    }
+    public String getName(){
+        return  name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+    public Trainer getTrainer(){
+        return  trainer;
+    }
+    public void setTrainer() {
+        this.trainer = trainer;
+    }
+
+    public Torwart getTorwart(){
+        return  torwart;
+    }
+    public void setTorwart() {
+        this.torwart = torwart;
+    }
+
+    public ArrayList<Spieler> getSpielerListe(){
+        return  spielerListe;
+    }
+    public void setSpielerListe() {
+        this.spielerListe = spielerListe;
+    }
+
+   // public int getMotivation(){
+    //   return ;
+
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Fussball/src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    private String name;
+    private int alter;
+
+    public Person (String name, int alter) {
+        this.name = name;
+        this.alter = alter;
+    }
+
+
+    public String getName(){
+        return  name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+
+    public int getAlter(){
+        return  alter;
+    }
+    public void setAlter() {
+        this.alter = alter;
+    }
+
+
+
+
+
+}
