3dcc73a4b04f7ead2f8e180d5c19de5d32242cf0
milane2 <milane2@schule.bremen.de>
2021-07-07 09:23:57 +0200

    aufgabe 4 vom Projekt Zuul

--- Zuul/src/Spiel.java
+++ Zuul/src/Spiel.java
@@ -4,47 +4,60 @@
  *  Adventure-Game. Ein Spieler kann sich in einer Umgebung bewegen,
  *  mehr nicht. Das Spiel sollte auf jeden Fall ausgebaut werden,
  *  damit es interessanter wird!
- * 
+ *
  *  Zum Spielen muss eine Instanz dieser Klasse erzeugt werden und
  *  an ihr die Methode "spielen" aufgerufen werden.
- * 
+ *
  *  Diese Instanz erzeugt und initialisiert alle anderen Objekte
  *  der Anwendung: Sie legt alle R�ume und einen Parser an und
  *  startet das Spiel. Sie wertet auch die Befehle aus, die der
  *  Parser liefert, und sorgt f�r ihre Ausf�hrung.
- * 
+ *
  * @author  Michael K�lling und David J. Barnes
  * @version 2008.03.30
  */
 
-public class Spiel 
+public class Spiel
 {
     private Parser parser;
     private Raum aktuellerRaum;
-        
+
     /**
      * Erzeuge ein Spiel und initialisiere die interne Raumkarte.
      */
-    public Spiel() 
+    public Spiel()
     {
         raeumeAnlegen();
         parser = new Parser();
     }
 
+    public void raumInfoAusgeben(){
+
+        System.out.println("Sie sind " + aktuellerRaum.gibBeschreibung());
+        System.out.print("Ausgänge: ");
+        if(aktuellerRaum.nordausgang != null)
+            System.out.print("north ");
+        if(aktuellerRaum.ostausgang != null)
+            System.out.print("east ");
+        if(aktuellerRaum.suedausgang != null)
+            System.out.print("south ");
+        if(aktuellerRaum.westausgang != null)
+            System.out.print("west ");
+        System.out.println();
+    }
     /**
-     * Erzeuge alle R�ume und verbinde ihre Ausg�nge miteinander.
+     * Erzeuge alle Räume und verbinde ihre Ausgänge miteinander.
      */
-    private void raeumeAnlegen()
-    {
+    private void raeumeAnlegen() {
         Raum lichtung, waldstueck, taverne, hexenhaus, dorfplatz;
-      
+
         // die R�ume erzeugen
         lichtung = new Raum("auf einer Lichtung, umgeben von dunklen Tannen");
         waldstueck = new Raum("im dunklen Wald");
         taverne = new Raum("in der Taverne, mit zwielichtigen Gestalten an der Theke");
         hexenhaus = new Raum("im Hexenhaus");
         dorfplatz = new Raum("auf dem Dorfplatz");
-        
+
         // die Ausg�nge initialisieren
         lichtung.setzeAusgaenge(null, null, null, waldstueck);
         waldstueck.setzeAusgaenge(null, lichtung, dorfplatz, null);
@@ -58,15 +71,14 @@ public class Spiel
      * Die Hauptmethode zum Spielen. L�uft bis zum Ende des Spiels
      * in einer Schleife.
      */
-    public void spielen() 
-    {            
+    public void spielen() {
         willkommenstextAusgeben();
 
         // Die Hauptschleife. Hier lesen wir wiederholt Befehle ein
         // und f�hren sie aus, bis das Spiel beendet wird.
-                
+
         boolean beendet = false;
-        while (! beendet) {
+        while (!beendet) {
             Befehl befehl = parser.liefereBefehl();
             beendet = verarbeiteBefehl(befehl);
         }
@@ -76,24 +88,13 @@ public class Spiel
     /**
      * Einen Begr��ungstext f�r den Spieler ausgeben.
      */
-    private void willkommenstextAusgeben()
-    {
+    private void willkommenstextAusgeben() {
         System.out.println();
         System.out.println("Willkommen zu Zuul!");
         System.out.println("Entdecke die Welt von Zuul. Doch Vorsicht, �berall lauern Gefahren!");
         System.out.println("Tippen sie 'help', wenn Sie Hilfe brauchen.");
         System.out.println();
-        System.out.println("Sie sind " + aktuellerRaum.gibBeschreibung());
-        System.out.print("Ausg�nge: ");
-        if(aktuellerRaum.nordausgang != null)
-            System.out.print("north ");
-        if(aktuellerRaum.ostausgang != null)
-            System.out.print("east ");
-        if(aktuellerRaum.suedausgang != null)
-            System.out.print("south ");
-        if(aktuellerRaum.westausgang != null)
-            System.out.print("west ");
-        System.out.println();
+        raumInfoAusgeben();
     }
 
     /**
@@ -101,12 +102,11 @@ public class Spiel
      * @param befehl Der zu verarbeitende Befehl.
      * @return 'true', wenn der Befehl das Spiel beendet, 'false' sonst.
      */
-    private boolean verarbeiteBefehl(Befehl befehl) 
-    {
+    private boolean verarbeiteBefehl(Befehl befehl) {
         boolean moechteBeenden = false;
 
         if(befehl.istUnbekannt()) {
-            System.out.println("Ich wei� nicht, was Sie meinen...");
+            System.out.println("Ich weiß nicht, was Sie meinen...");
             return false;
         }
 
@@ -128,12 +128,11 @@ public class Spiel
      * Hier geben wir eine etwas alberne und unklare Beschreibung
      * aus, sowie eine Liste der Befehlsw�rter.
      */
-    private void hilfstextAusgeben() 
-    {
+    private void hilfstextAusgeben() {
         System.out.println("Sie haben sich verlaufen. Sie sind allein.");
         System.out.println("Sie irren in der Welt von Zuul herum.");
         System.out.println();
-        System.out.println("Ihnen stehen folgende Befehle zur Verf�gung:");
+        System.out.println("Ihnen stehen folgende Befehle zur Verfügung:");
         System.out.println("   go quit help");
     }
 
@@ -142,11 +141,10 @@ public class Spiel
      * wechsele in den neuen Raum, ansonsten gib eine Fehlermeldung
      * aus.
      */
-    private void wechsleRaum(Befehl befehl) 
-    {
+    private void wechsleRaum(Befehl befehl) {
         if(!befehl.hatZweitesWort()) {
-        	// Gibt es kein zweites Wort, wissen wir nicht, wohin...
-            System.out.println("Wohin m�chten Sie gehen?");
+            // Gibt es kein zweites Wort, wissen wir nicht, wohin...
+            System.out.println("Wohin möchten Sie gehen?");
             return;
         }
 
@@ -168,21 +166,11 @@ public class Spiel
         }
 
         if (naechsterRaum == null) {
-            System.out.println("Dort ist keine T�r!");
+            System.out.println("Dort ist kein Tor!");
         }
         else {
             aktuellerRaum = naechsterRaum;
-            System.out.println("Sie sind " + aktuellerRaum.gibBeschreibung());
-            System.out.print("Ausg�nge: ");
-            if(aktuellerRaum.nordausgang != null)
-                System.out.print("north ");
-            if(aktuellerRaum.ostausgang != null)
-                System.out.print("east ");
-            if(aktuellerRaum.suedausgang != null)
-                System.out.print("south ");
-            if(aktuellerRaum.westausgang != null)
-                System.out.print("west ");
-            System.out.println();
+            raumInfoAusgeben();
         }
     }
 
@@ -191,8 +179,7 @@ public class Spiel
      * ob das Spiel wirklich beendet werden soll.
      * @return 'true', wenn der Befehl das Spiel beendet, 'false' sonst.
      */
-    private boolean beenden(Befehl befehl) 
-    {
+    private boolean beenden(Befehl befehl) {
         if(befehl.hatZweitesWort()) {
             System.out.println("Was soll beendet werden?");
             return false;
