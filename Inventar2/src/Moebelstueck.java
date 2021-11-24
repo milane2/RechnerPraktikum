73b44c75526ab9b566beb7a92fee9a842fcbc3c4
milane2 <milane2@schule.bremen.de>
2021-11-24 12:48:01 +0100

    Neu Inventar2

---
+++ Inventar2/src/Moebelstueck.java
@@ -0,0 +1,16 @@
+public class Moebelstueck {
+
+    private String inventarNr;
+    private String bezeichnung;
+    private  String beschreibung;
+    private  String material;
+    private double gewicht;
+
+    public Moebelstueck(String inventarNr,String bezeichnung){
+        this.inventarNr = inventarNr;
+        this.bezeichnung = bezeichnung;
+        this.beschreibung = beschreibung;
+        this.material = material;
+        this.gewicht = gewicht;
+    }
+}
